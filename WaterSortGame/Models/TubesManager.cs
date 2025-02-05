700a42341c2f5394ebdc8547ad054bb01366f23b
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-02-05 18:08:16 +0100

    Fixing TubesManager - didn't properly reset extra tubes on level restart.

--- WaterSortGame/Models/TubesManager.cs
+++ WaterSortGame/Models/TubesManager.cs
@@ -130,131 +130,9 @@ namespace WaterSortGame.Models
                 }
             }
         }
-        //static TubesList()
-        //{
-        //    MaximumExtraTubes = Settings.Default.MaximumExtraTubes;
-        //}
-
-        //public static ObservableCollection<Tube> _tubes = new ObservableCollection<Tube>()
-        //{ // posunul jsem IDcka zkumavek o jedna. spravit!
-        //    new Tube(0),
-        //    new Tube(1),
-        //    new Tube(2),
-        //    new Tube(3),
-        //    new Tube(4),
-        //    new Tube(5),
-        //    new Tube(6),
-        //    new Tube(7),
-        //    new Tube(8),
-        //    new Tube(9),
-        //    new Tube(10),
-        //    new Tube(11),
-        //    new Tube(12),
-        //    new Tube(13),
-        //};
-
-        //private static readonly ObservableCollection<Tube> _defaultTubes = new ObservableCollection<Tube>()
-        //{ // posunul jsem IDcka zkumavek o jedna. spravit!
-        //    new Tube(9, 2, 4, 1),
-        //    new Tube(3, 8, 11, 5),
-        //    new Tube(9, 11, 11, 12),
-        //    new Tube(3, 3, 2, 5),
-        //    new Tube(1, 7, 6, 10),
-        //    new Tube(3, 4, 7, 4),
-        //    new Tube(2, 8, 5, 10),
-        //    new Tube(6, 1, 2, 9),
-        //    new Tube(11, 10, 7, 6),
-        //    new Tube(5, 7, 10, 4),
-        //    new Tube(8, 12, 6, 12),
-        //    new Tube(1, 12, 8, 9),
-        //    //new Tube(0, 0, 0, 0),
-        //    //new Tube(0, 0, 0, 0)
-        //    new Tube(),
-        //    new Tube(),
-        //};
-
-        //public static ObservableCollection<Tube> GetDefaultValues()
-        //{
-        //    return new ObservableCollection<Tube>()
-        //    { // posunul jsem IDcka zkumavek o jedna. spravit!
-        //        new Tube(9, 2, 4, 1),
-        //        new Tube(3, 8, 11, 5),
-        //        new Tube(9, 11, 11, 12),
-        //        new Tube(3, 3, 2, 5),
-        //        new Tube(1, 7, 6, 10),
-        //        new Tube(3, 4, 7, 4),
-        //        new Tube(2, 8, 5, 10),
-        //        new Tube(6, 1, 2, 9),
-        //        new Tube(11, 10, 7, 6),
-        //        new Tube(5, 7, 10, 4),
-        //        new Tube(8, 12, 6, 12),
-        //        new Tube(1, 12, 8, 9),
-        //        //new Tube(0, 0, 0, 0),
-        //        //new Tube(0, 0, 0, 0)
-        //        new Tube(),
-        //        new Tube(),
-        //    };
-        //}
-
-        
         public static ObservableCollection<Tube> SavedStartingTubes = new ObservableCollection<Tube>();
-
-        //private int countTubes;
-        //public int CountTubes
-        //{
-        //    get
-        //    {
-        //        return (int)Math.Ceiling((decimal)_tubes.Count / 2);
-        //    }
-        //    //set
-        //    //{
-        //    //    countTubes = value;
-        //    //}
-        //}
-        //public static void StartNewLevel(bool regenerate = false)
         public static void StartNewLevel()
         {
-            //if (regenerate is true)
-            //{
-            //    _tubes.Clear();
-            //}
-
-            //if (false)
-            //{ // posunul jsem IDcka zkumavek o jedna. spravit!
-            //    _tubes.Add(new Tube(9, 2, 4, 1));
-            //    _tubes.Add(new Tube(3, 8, 11, 5));
-            //    _tubes.Add(new Tube(9, 11, 11, 12));
-            //    _tubes.Add(new Tube(3, 3, 2, 5));
-            //    _tubes.Add(new Tube(1, 7, 6, 10));
-            //    _tubes.Add(new Tube(3, 4, 7, 4));
-            //    _tubes.Add(new Tube(2, 8, 5, 10));
-            //    _tubes.Add(new Tube(6, 1, 2, 9));
-            //    _tubes.Add(new Tube(11, 10, 7, 6));
-            //    _tubes.Add(new Tube(5, 7, 10, 4));
-            //    _tubes.Add(new Tube(8, 12, 6, 12));
-            //    _tubes.Add(new Tube(1, 12, 8, 9));
-            //    _tubes.Add(new Tube());
-            //    _tubes.Add(new Tube());
-            //    _tubes.Add(new Tube()); // extra
-            //}
-            //else
-            //{
-            //    _tubes.Add(new Tube(1, 1, 1, 1));
-            //    _tubes.Add(new Tube(2, 2, 2, 2));
-            //    _tubes.Add(new Tube(3, 3, 3, 3));
-            //    _tubes.Add(new Tube(4, 4, 4, 4));
-            //    _tubes.Add(new Tube(5, 5, 5, 5));
-            //    _tubes.Add(new Tube(6, 6, 6, 6));
-            //    _tubes.Add(new Tube(7, 7, 7, 7));
-            //    _tubes.Add(new Tube(8, 8, 8, 8));
-            //    _tubes.Add(new Tube(9, 9, 9, 9));
-            //    _tubes.Add(new Tube(10, 10, 10, 10));
-            //    _tubes.Add(new Tube(11, 11, 11, 12));
-            //    _tubes.Add(new Tube(12, 12, 12, 11));
-            //    _tubes.Add(new Tube());
-            //    _tubes.Add(new Tube());
-            //}
-
             GenerateNewTubes();
         }
 
@@ -268,6 +146,7 @@ namespace WaterSortGame.Models
         }
         public static void RestartLevel()
         {
+            ExtraTubes = 0; // resets how much extra tubes has been added
             //SavedStartingTubes?.Clear();
             Tubes?.Clear();
             foreach (var tube in SavedStartingTubes)
@@ -379,18 +258,5 @@ namespace WaterSortGame.Models
                 SavedStartingTubes.Add((Tube)tube.DeepCopy());
             }
         }
-
-        //public static ObservableCollection<Tube> GetTubes()
-        //{
-        //    if (Tubes.Count == 0)
-        //        StartNewLevel();
-
-        //    return Tubes;
-        //}
-        //private static ObservableCollection<Tube> tubesProperty;
-        //public static ObservableCollection<Tube> TubesProperty
-        //{
-        //    get { return Tubes; }
-        //}
     }
 }
