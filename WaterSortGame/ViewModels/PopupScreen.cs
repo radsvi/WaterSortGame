4058aef1d6ec707e9ffdfc4592d21192eb513969
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-02-16 22:03:07 +0100

    Moving classes inherited from PopupScreenBase all to one file.

---
+++ WaterSortGame/ViewModels/PopupScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterSortGame.ViewModels
+{
+    internal class PopupScreenBase : ViewModelBase
+    {
+        public MainWindowVM MainWindowVM { get; set; }
+        public PopupScreenBase(object viewModel)
+        {
+            MainWindowVM = (MainWindowVM)viewModel;
+        }
+    }
+    internal class HelpVM : PopupScreenBase
+    {
+        public HelpVM(object viewModel) : base(viewModel) { }
+    }
+    internal class LevelCompleteVM : PopupScreenBase
+    {
+        public LevelCompleteVM(object viewModel) : base(viewModel) { }
+    }
+    class LoadLevelVM : PopupScreenBase
+    {
+        public LoadLevelVM(object viewModel) : base(viewModel)
+        {
+            MainWindowVM = (MainWindowVM)viewModel;
+            MainWindowVM.LoadLevelScreen();
+        }
+    }
+    internal class NewLevelVM : PopupScreenBase
+    {
+        public NewLevelVM(object viewModel) : base(viewModel) { }
+    }
+    internal class RestartLevelVM : PopupScreenBase
+    {
+        public RestartLevelVM(object viewModel) : base(viewModel) { }
+    }
+    class GameSavedVM : PopupScreenBase
+    {
+        public GameSavedVM(object viewModel) : base(viewModel) { }
+    }
+}
